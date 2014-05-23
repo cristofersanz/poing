84215e85421b13c5a62b69af9cff3741eebe17f3
Cristofer Sanz Blasco <cristofercri@gmail.com>
2014-05-23 21:25:16 +0200

    Añadidos graficos, menu de seleccion, pausa... Refactorizado.

--- src/main/java/vv12/pong/Game.java
+++ src/main/java/vv12/pong/Game.java
@@ -1,111 +1,188 @@
 package vv12.pong;
 
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Input;
-import vv12.pong.graficos.*;
-import vv12.pong.logica.*;
-import java.util.Properties;
+import org.newdawn.slick.*;
+import vv12.pong.graficos.Pantalla;
+import vv12.pong.logica.Bola;
+import vv12.pong.logica.Jugador;
+
 import static vv12.pong.utiles.Utiles.conf;
 
 /**
  * A game using Slick2d
  */
-public class Game extends BasicGame {
+public class Game extends BasicGame implements KeyListener {
 
-    private static final int WIDTH = Integer.valueOf(conf("pantalla.width"));
-    private static final int HEIGHT = Integer.valueOf(conf("pantalla.height"));
-    private static final int J1_POS_X = (int) (0.05 * WIDTH);    
-    private static final int J2_POS_X = (int) (0.95 * WIDTH);   
-    private static final int POS_Y = (HEIGHT - Integer.valueOf(conf("pala.altura"))) / 2;
-    private static final int TIEMPO_RESET = Integer.valueOf(conf("tiempo_reset"));
-    private static final boolean DEBUG = Boolean.valueOf(conf("debug"));
-
+    /* COMPONENTES JUEGO */
     private Jugador jugador1;
     private Jugador jugador2;
     private Bola bola;
     private Input input;
+
+    /* FLAGS */
     private boolean reset;
+    private boolean pause = false;
+    private boolean menu = true;
+    private int opcionMenu = 1;
+    private int gameOver = 0;
     private int timer;
 
     public Game() {
         super("Pong original!");
     }
 
-    public void render(GameContainer container, Graphics g) throws SlickException {
-        Pantalla.pintarJugador(g,jugador1);
-        Pantalla.pintarJugador(g,jugador2);
-        Pantalla.pintarBola(g,bola);
-        Pantalla.pintarRed(g);
-        Pantalla.pintarMarcadores(g,jugador1,jugador2);
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        jugador1 = new Jugador((int) (0.05 * Integer.valueOf(conf("pantalla.width"))),
+                (Integer.valueOf(conf("pantalla.height")) - Integer.valueOf(conf("pala.altura"))) / 2);
+        jugador2 = new Jugador((int) (0.95 * Integer.valueOf(conf("pantalla.width"))),
+                (Integer.valueOf(conf("pantalla.height")) - Integer.valueOf(conf("pala.altura"))) / 2);
+        bola = new Bola();
+        input = new Input(Integer.valueOf(conf("pantalla.height")));
 
-        if (DEBUG){
-            g.drawString("X: " + bola.getPosX() + " || Y: " + bola.getPosY() + " || DIR: " +
-                bola.getDir(), 100,10);
-        }
+        /* Esperar tiempo hasta iniciar partida */
+        timerOn();
     }
 
     @Override
-    public void init(GameContainer container) throws SlickException {
-        jugador1 = new Jugador(J1_POS_X,POS_Y);
-        jugador2 = new Jugador(J2_POS_X,POS_Y);
-        bola = new Bola();
-        input = new Input(HEIGHT);
-        timerOn();
-    }
-
-    public void reinit(){
-        bola = new Bola();
-        timerOn();
-    }
-
-    public void timerOn() {
-        timer = 0;
-        reset = true;
-    }
-
-    public void timerOff() {
-        reset = false;
-    }
-
-    public void timer(int delta) throws SlickException {
-        if (reset) {
-            timer += delta;
-        }
-
-        if (timer >= TIEMPO_RESET) {
-            timerOff();
+    public void keyPressed(int key, char c) {
+        if (menu) {
+            switch (key) {
+                case Input.KEY_UP:
+                    if (opcionMenu > 1)         /* Dificultad fácil */
+                        opcionMenu -= 1;
+                    break;
+                case Input.KEY_DOWN:
+                    if (opcionMenu < 3)         /* Dificultad difícil */
+                        opcionMenu += 1;
+                    break;
+                case Input.KEY_ENTER:
+                    seleccionarDificultad();
+                    break;
+            }
+        } else {
+            if (key == Input.KEY_ENTER) {
+                pause();
+            }
         }
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
-        jugador1.jugar(1,input);
-        jugador2.jugar(2,input);
-        
-        timer(delta);
+        if (!pause && !menu && gameOver == 0) {
 
-        switch (bola.calcularRebotes(jugador1.getPala(),jugador2.getPala(),reset)){
-            case 1:     /* Gana jugador 1 */
-                jugador1.incrementarPuntuacion();
-                reinit();
+            /* Escucha teclas de movimiento y mueve */
+            jugador1.jugar(1, input);
+            jugador2.jugar(2, input);
+
+            /* Disminuye tiempo del timer si está en marcha */
+            timer(delta);
+
+            /* Mueve bola si no está iniciando partida o juego */
+            if (!reset) {
+                switch (bola.calcularRebotes(jugador1.getPala(), jugador2.getPala())) {
+                    case 1:     /* Gana jugador 1 */
+                        jugador1.incrementarPuntuacion();
+                        if (jugador1.getPuntuacion() >= Integer.valueOf(conf("num_partidas")))
+                            gameOver = 1;
+                        else
+                            reinit();
+                        break;
+                    case 2:     /* Gana jugador 2 */
+                        jugador2.incrementarPuntuacion();
+                        if (jugador2.getPuntuacion() >= Integer.valueOf(conf("num_partidas")))
+                            gameOver = 2;
+                        else
+                            reinit();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        Pantalla.pintarJugador(g, jugador1);
+        Pantalla.pintarJugador(g, jugador2);
+        Pantalla.pintarBola(g, bola);
+        Pantalla.pintarRed(g);
+        Pantalla.pintarMarcadores(g, jugador1, jugador2);
+
+        if (pause) {
+            Pantalla.pintarPause(g);
+        }
+
+        if (menu) {
+            Pantalla.pintarMenu(g, opcionMenu);
+        }
+
+        if (gameOver != 0) {
+            Pantalla.pintarGameOver(g, gameOver);
+        }
+
+        if (Boolean.valueOf(conf("debug"))) {
+            g.drawString("X: " + bola.getPosX() + " || Y: " + bola.getPosY() + " || DIR: " +
+                    bola.getDir() + " || PAUSE: " + pause, 100, 10);
+        }
+    }
+
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new Game());
+        app.setDisplayMode(Integer.valueOf(conf("pantalla.width")), Integer.valueOf(conf("pantalla.height")), false);
+        app.setForceExit(false);
+        app.start();
+    }
+
+    private void reinit() {
+        /* Centra bola y espera para empezar partida */
+        bola = new Bola();
+        timerOn();
+    }
+
+    private void timerOn() {
+        timer = 0;
+        reset = true;
+    }
+
+    private void timerOff() {
+        reset = false;
+    }
+
+    private void pause() {
+        pause = !pause;
+    }
+
+    private void timer(int delta) {
+        /* Añade tiempo si timer activo */
+        if (reset) {
+            timer += delta;
+        }
+
+        /* Desactiva timer si tiempo superado */
+        if (timer >= Integer.valueOf(conf("tiempo_reset"))) {
+            timerOff();
+        }
+    }
+
+    private void seleccionarDificultad() {
+        switch (opcionMenu) {
+            case 1:     /* Facil */
+                conf("pala.altura", conf("facil.pala.altura"));
+                conf("bola.velocidad", conf("facil.bola.velocidad"));
                 break;
-            case 2:     /* Gana jugador 2 */
-                jugador2.incrementarPuntuacion();
-                reinit();
+
+            case 2:     /* Normal */
+                conf("pala.altura", conf("normal.pala.altura"));
+                conf("bola.velocidad", conf("normal.bola.velocidad"));
+                break;
+
+            case 3:     /* Dificil */
+                conf("pala.altura", conf("dificil.pala.altura"));
+                conf("bola.velocidad", conf("dificil.bola.velocidad"));
                 break;
             default:
                 break;
         }
-    }
-    
-    public static void main(String[] args) throws SlickException {
-        AppGameContainer app = new AppGameContainer(new Game());
-        app.setDisplayMode(WIDTH, HEIGHT, false);
-        app.setForceExit(false);
-        app.start();
+        menu = false;
     }
 }
